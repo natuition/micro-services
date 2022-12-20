b3f6c064019cfde26498bb6cd498a79e9c3cb1ab
Vincent LAMBERT <v.lambert@natuition.com>
2022-12-20 16:53:10 +0100

    test weed_type ok

---
+++ data_gathering/tests/routes/test_weed_type.py
@@ -0,0 +1,49 @@
+from app.api.database.db import weed_types
+from sqlalchemy import insert
+from sqlalchemy.dialects import mysql
+
+
+def test_create_weed_type(client):
+    response = client.post(
+        "/api/v1/data_gathering/weed_type",
+        json={
+            "label": "Daisy"
+        },
+    )
+    assert response.status_code == 201
+    print("this will execute")
+    assert response.json() == {"id": 1, "label": "Daisy"}
+
+
+def test_get_list_weeds_types_empty(client):
+    response = client.get("/api/v1/data_gathering/weeds_types")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_get_list_weeds_types_with_one(client, cursor_cnx):
+    query = str(
+        insert(weed_types)
+        .values(label="Daisy")
+        .compile(dialect=mysql.dialect(), compile_kwargs={"literal_binds": True})
+    )
+    cursor_cnx[0].execute(query)
+    cursor_cnx[1].commit()
+    response = client.get("/api/v1/data_gathering/weeds_types")
+    assert response.status_code == 200
+    assert response.json() == [{"id": 1, "label": "Daisy"}]
+
+
+def test_get_list_weeds_types_with_multiple(client, cursor_cnx):
+    for weed_name in ["Daisy", "Plantain"]:
+        query = str(
+            insert(weed_types)
+            .values(label=weed_name)
+            .compile(dialect=mysql.dialect(), compile_kwargs={"literal_binds": True})
+        )
+        cursor_cnx[0].execute(query)
+        cursor_cnx[1].commit()
+    response = client.get("/api/v1/data_gathering/weeds_types")
+    assert response.status_code == 200
+    assert response.json() == [{"id": 1, "label": "Daisy"}, {
+        "id": 2, "label": "Plantain"}]
