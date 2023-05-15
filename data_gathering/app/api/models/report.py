00a86fd9fbe070ab314eb709644bc446574d4397
Vincent LAMBERT <v.lambert@natuition.com>
2023-05-15 15:19:38 +0200

    remove unsused import and add report module

--- data_gathering/app/api/models/report.py
+++ data_gathering/app/api/models/report.py
@@ -1,13 +1,7 @@
-from typing import Optional
 from pydantic import BaseModel, Field
-from datetime import datetime
 from app.api.models.session import SessionOut
 from app.api.models.field import FieldOut
-from app.api.models.extracted_weed import ExtractedWeedOut
 from app.api.models.gps_point import GPSPointOut
-from app.api.models.weed_type import WeedTypeOut
-from app.api.models.field_corner import FieldCornerOut
-from app.api.models.point_of_path import PointOfPathOut
 
 class ExtractedWeedWithGPSPointWithWeedTypeOut(BaseModel):
     latitude: float = Field(example=46.157638375739220000)
