19858a1cc404b8752b69233a0cca7bdf8e4f83c1
Natuition <49873359+natuition@users.noreply.github.com>
2023-09-22 11:26:52 +0200

    Add attribut to set valid token time

--- data_gathering/app/auth/token.py
+++ data_gathering/app/auth/token.py
@@ -2,6 +2,9 @@ import time
 from app.api.models.customer import CustomerOut
 
 class Token:
+
+    SECONDS_BEFORE_TOKEN_EXPIRATION = 60 * 60 * 24
+
     def __init__(self, payload = None) -> None:
         if payload is not None:
             self.customer_id = payload["customer_id"]
@@ -11,7 +14,7 @@ class Token:
     def generate(self, customer: CustomerOut):
         self.customer_id : str = customer.id,
         self.customer_role : str = customer.role
-        self.expires : str = time.time() + 6000
+        self.expires : str = time.time() + self.SECONDS_BEFORE_TOKEN_EXPIRATION
         return self
 
     def get_str(self) -> str:
